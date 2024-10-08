eb101024fa6c5f5f9023561e4ad84bbcd8c02357
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-10-08 14:22:08 -0400

    trying something

---
+++ app/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const recipeProp = defineProps({
+    name: String,
+    cuisine: String,
+})
+</script>
+<template>
+    <div>
+        <div>
+            <h3>
+                {{ recipeProp.name }}
+            </h3>
+            <p>cuisine: {{ recipeProp.cuisine }}</p>
+        </div>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref, onMounted} from "vue";
+
+const recipes = ref([]);
+onMounted(async() => {
+    try{
+        const response = await fetch(
+        "https://dummyjson.com/recipes"
+        );
+
+        recipes.value = await response.json();
+        console.log(recipes.value);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+</script>
+<template>
+    <main>
+        <h2>Recipes</h2>
+        <ul>
+            <li
+                v-for="r in recipes" :key="r.name">
+                <Card 
+                    :name="r.name"
+                    :cuisine="r.cuisine"
+                />
+            </li>
+        </ul>
+    </main>
+</template>
