8d41a4f9683400d381c9e4b12a0d87a01cc51d5a
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-11-11 22:09:52 -0500

    fixed spacing on recipes

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -13,31 +13,30 @@ const recipeProp = defineProps({
 </script>
 <template>
     <div :id="recipeProp.id" class="recipe-card__container">
-
+        
         <div class="row recipe-card__top-container">
-            <div class="col-lg-7 recipe-card__img-container ">
+            <div class=" col-lg-4 col-md-6 ecipe-card__img-container ">
                 <img class="recipe-card__img img-fluid" :src="recipeProp.image">
             </div>
-            <div class="col-lg-5 recipe-card__top-text">
+            <div class=" col-lg-8 col-md-6 recipe-card__top-text">
                 <h2 class="recipe-card__name">{{ recipeProp.name }}</h2>
                 <div class="recipe-card__description">
                     <p>Cook Time: {{ recipeProp.time }}</p>
                     <p>Servings: {{ recipeProp.servings }}</p>
                 </div>
-                
-                <div class="recipe-card__ingredients">
-                    <h4>Ingredients</h4>
-                    <ul class="ingredients__list">
-                        <li class="ingredients__item" v-for="ing in ingrediants" :key="ing[0]">{{ ing }}</li>
-                    </ul>
-                </div>
             </div>
         </div>
-
-        <div class="recipe-card__bottom-container">
-
-            <div class="recipe-card__directions">
-                <h4>Directions</h4>
+        
+        <div class=" row recipe-card__bottom-container">
+            
+            <div class="col-lg-4 recipe-card__ingredients">
+                <h4 class="recipe-card__title">Ingredients</h4>
+                <ul class="ingredients__list">
+                    <li class="ingredients__item" v-for="ing in ingrediants" :key="ing[0]">{{ ing }}</li>
+                </ul>
+            </div>
+            <div class="col-lg-8 recipe-card__directions">
+                <h4 class="recipe-card__title">Directions</h4>
                 <ol class="directions__list">
                     <li class="directions__item" v-for="dir in directions" :key="dir[0]">{{ dir }}</li>
                 </ol>
