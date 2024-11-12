a515e55087ccf054824fb5a1b645f5dbb15e0b66
Brittni Rohrer <brittnirohrer@Brittnis-MacBook-Air.local>
2024-11-12 01:10:16 -0500

    chng card again

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -15,10 +15,10 @@ const recipeProp = defineProps({
     <div :id="recipeProp.id" class="recipe-card__container">
         
         <div class="row recipe-card__top-container">
-            <div class=" col-lg-4 col-md-6 ecipe-card__img-container ">
+            <div class="col-md-6 ecipe-card__img-container ">
                 <img class="recipe-card__img img-fluid" :src="recipeProp.image">
             </div>
-            <div class=" col-lg-8 col-md-6 recipe-card__top-text">
+            <div class="col-md-6 recipe-card__top-text">
                 <h2 class="recipe-card__name">{{ recipeProp.name }}</h2>
                 <div class="recipe-card__description">
                     <p>Cook Time: {{ recipeProp.time }}</p>
